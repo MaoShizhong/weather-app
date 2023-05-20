c60a74e8a5a873fa60fd1a813c3c4cb8d4aaa832
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-05-21 00:22:34 +0100

    Add unit change btns

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,12 @@ import '../images/rain.webp';
 import '../images/snow.webp';
 
 const form = document.querySelector('form');
+const unitBtns = document.querySelectorAll('#units > button');
+
 form.addEventListener('submit', DisplayController.showForecast);
+unitBtns.forEach((btn) => {
+    btn.addEventListener('click', DisplayController.changeUnits);
+});
 
 // * initialise on load without transition animation
 document.querySelector('button').click();
--- src/js/modules/display.js
+++ src/js/modules/display.js
@@ -16,26 +16,33 @@ export class DisplayController {
         input.value = '';
     }
 
-    static updateMainInfo() {
+    static updateMainInfo(inCelcius) {
         document.querySelector('#location').textContent = `${Forecast.location}`;
         document.querySelector('#region').textContent = `${Forecast.region}`;
         document.querySelector('#country').textContent = `${Forecast.country}`;
     }
 
-    static updateWeek() {
+    static updateWeek(inCelcius = true) {
         const days = document.querySelectorAll('.week');
 
         // ? i + 1 as current day will be displayed large
-        days.forEach((day, i) => DisplayController.updateDay(day, i + 1));
+        days.forEach((day, i) => DisplayController.updateDay(day, i + 1, inCelcius));
     }
 
-    static updateDay(day, i) {
+    static updateDay(day, i, inCelsius = true) {
         const data = Forecast.getDaySummary(i);
-        console.log(data[2]);
+
+        const unit = inCelsius ? 'C' : 'F';
+        if (!inCelsius) {
+            data[2] = Math.round((data[2] * 9) / 5 + 32);
+            data[3] = Math.round((data[3] * 9) / 5 + 32);
+            data[4] = Math.round((data[4] * 9) / 5 + 32);
+        }
+
         day.querySelector('img').src = data[0];
         day.querySelector('h3').textContent = data[1];
-        day.querySelector('.average-temp').textContent = `${data[2]} \u00B0C`;
-        day.querySelector('.min-max').textContent = `${data[3]} - ${data[4]} \u00B0C`;
+        day.querySelector('.average-temp').textContent = `${data[2]} \u00B0${unit}`;
+        day.querySelector('.min-max').textContent = `${data[3]} - ${data[4]} \u00B0${unit}`;
 
         // * update background image for section
         day.dataset.weather = data[5].includes('sunny')
@@ -49,5 +56,31 @@ export class DisplayController {
             : data[5].includes('overcast')
             ? 'overcast'
             : 'storm';
+
+        if (/sunny/.test(data[5])) {
+            day.dataset.weather = 'sunny';
+        } else if (/ice|snow|blizzard|sleet|(freez)(?!.+fog)/.test(data[5])) {
+            day.dataset.weather = 'snow';
+        } else if (/rain|shower|drizzle/.test(data[5])) {
+            day.dataset.weather = 'rain';
+        } else if (/cloudy|mist/.test(data[5])) {
+            day.dataset.weather = 'cloudy';
+        } else if (/fog|overcast/.test(data[5])) {
+            day.dataset.weather = 'overcast';
+        } else {
+            day.dataset.weather = 'storm';
+        }
+    }
+
+    static changeUnits(e) {
+        const inCelsius = e.target.value === 'celsius';
+
+        DisplayController.updateMainInfo(inCelsius);
+        DisplayController.updateWeek(inCelsius);
+
+        // * adjust btn visuals
+        const current = document.querySelector('.current-btn');
+        current.classList.remove('current-btn');
+        e.target.classList.add('current-btn');
     }
 }
